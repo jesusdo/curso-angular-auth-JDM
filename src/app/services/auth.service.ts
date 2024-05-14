e27aa96fd97e529487566351d163372d0fd7e669
jesusdo <108779318+jesusdo@users.noreply.github.com>
2024-05-13 21:41:45 -0700

    consumir api

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '@environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {email, password});
+  };
+}
